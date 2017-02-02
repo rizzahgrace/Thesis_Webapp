b9afa5e6858dbdae22bedbce94af38af89446f28
Rizzah <rizzah_2113@yahoo.com>
2017-02-02 10:56:38 +0800

    Django Highcharts Sample chart works!

--- thesis/webapp/urls.py
+++ thesis/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-from webapp.views import DataListView
+from webapp.views import DataListView, BarView
 from rest_framework.urlpatterns import format_suffix_patterns
 from webapp import views
 
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^rawdatalist', views.rawdatalist, name='rawdatalist'),
     url(r'^rawdata', views.DataListView.as_view(), name='rawdata'),
     url(r'^chart', views.plot, name='chart'),
+    url(r'^bar', views.BarView.as_view(), name='bar'),
 ]
 
 urlpatterns += [
--- thesis/webapp/views.py
+++ thesis/webapp/views.py
@@ -12,9 +12,10 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from webapp.serializers import DataSerializer
 
+from highcharts.views import (HighChartsMultiAxesView, HighChartsPieView, HighChartsSpeedometerView, HighChartsHeatMapView, HighChartsPolarView)
 # Create your views here.
 def index(request):
-	return render(request, 'webapp/home.html')
+	return render(request, 'webapp/highchart_script.html')
 
 def csv(request):
 	if request.method == 'POST':
@@ -28,6 +29,8 @@ def csv(request):
 
 	return render(request, 'webapp/csv.html', {'form': form})
 
+#for using Chartit
+#error says Nonetype object not subscriptable
 def weather_chart_view(request):
 	#Step 1: Create a DataPool with the data we want to retrieve.
 	weatherdata = DataPool(
@@ -58,7 +61,7 @@ def weather_chart_view(request):
 
 class DataListView(APIView):
 	def get(self, request):
-		data = RawData.objects.values('winddir')
+		data = RawData.objects.values('winddir', 'rainin')
 		return Response(data)
 
 class JSONResponse(HttpResponse):
@@ -89,3 +92,108 @@ def plot(request, chartID = 'rawdatachart', chart_type = 'line', chart_height = 
 		{"name": 'Rain', "data": data['rainin']}
 		]
 	return render(request, 'webapp/highchart_script.html', {'chartID': chartID, 'title': title,'xAxis': xAxis, 'yAxis': yAxis, 'series':series})
+
+class BarView(HighChartsMultiAxesView):
+	title = 'Example Data Chart'
+	subtitle = ''
+	categories = ['Orange', 'Bananas', 'Apples']
+	chart_type = ''
+	chart = {'zoomType': 'xy'}
+	tooltip = {'shared': 'true'}
+	legend = {'layout': 'horizontal', 'align': 'left',
+			  'floating': 'true', 'verticalAlign': 'top',
+			  'y': -10, 'borderColor': '#e3e3e3'}
+
+	@property
+	def yaxis(self):
+		y_axis = [
+			{'labels': {'format': '{value} pz/sc ', 'style': {'color': '#f67d0a'}},
+			 'title': {'text': "Oranges", 'style': {'color': '#f67d0a'}},
+			 'opposite': 'true'},
+			{'gridLineWidth': 1,
+			 'title': {'text': "Bananas", 'style': {'color': '#3771c8'}},
+			 'labels': {'style': {'color': '#3771c8'}, 'format': '{value} euro'}},
+			{'gridLineWidth': 1,
+			 'title': {'text': "Apples", 'style': {'color': '#666666'}},
+			 'labels': {'format': '{value} pz', 'style': {'color': '#666666'}},
+			 'opposite': 'true'}
+		]
+		return y_axis
+
+	@property
+	def series(self):
+		series = [
+			{
+				'name': 'Orange',
+				'type': 'column',
+				'yAxis': 1,
+				'data': [90,44,55,67,4,5,6,3,2,45,2,3,2,45,5],
+				'tooltip': "{ valueSuffix: ' euro' }",
+				'color': '#3771c8'
+			},
+			{
+				'name': 'Bananas',
+				'type': 'spline',
+				'yAxis': 2,
+				'data': [12,34,34,34, 5,34,3,45,2,3,2,4,4,1,23],
+				'marker': { 'enabled': 'true' },
+				'dashStyle': 'shortdot',
+				'color': '#666666',
+				},
+			{
+				'name': 'Apples',
+				'type': 'spline',
+				'data': [12,23,23,23,21,4,4,76,3,66,6,4,5,2,3],
+				'color': '#f67d0a'
+			}
+		]
+		return series
+
+class AdvancedGraph(HighChartsMultiAxesView):
+	title = 'Advanced graph'
+	subtitle = 'params and query'
+	chart = {'zoomType': 'xy'}
+	tooltip = {'shared': 'true'}
+	legend = {
+		'layout': 'vertical',
+		'align': 'left',
+		'verticalAlign': 'top',
+		'y': 30
+	}
+
+	def get_data(self):
+		f = RawData.objects.all()
+		cursor = connection.cursor()
+		# cursor.execute("SELECT timestamp, winddir from rawdata", [f.pk])
+		graph = cursor.fetchall()
+		timestamp = list()
+		winddir = list()
+
+		for i in range(0, len(graph)):
+			timestamp.append(rows[i][9])
+			winddir.append(rows[i][3])
+
+		#### SERIES
+		self.serie = graph[0]
+
+		##### X LABELS
+		self.categories = graph[1]
+
+		##### Y AXIS DEFINITIONS
+		self.yaxis = {
+			'title': {
+				'text': 'Title 1'
+			},
+			'plotLines': [
+				{
+					'value': 0,
+					'width': 1,
+					'color': '#808080'
+				}
+			]
+		}
+
+		##### SERIES WITH VALUES
+		self.series = self.serie
+		data = super(AdvancedGraph, self).get_data()
+		return data
